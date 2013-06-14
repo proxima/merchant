4bb893befb434d36d56f4a4b0e4e37752834f1de
Christopher Lee <f4milyp4nt@gmail.com>
2013-06-14 19:14:01 -0400

    Adding facebook auth, menus on each side

---
+++ public/fb_auth.js
@@ -0,0 +1,36 @@
+// Additional JS functions here
+window.fbAsyncInit = function() {
+  FB.init({
+    appId      : '461623053929710', // App ID
+    channelUrl : '//merchant.botchris.com/channel.html', // Channel File
+    status     : true, // check login status
+    cookie     : true, // enable cookies to allow the server to access the session
+    xfbml      : true  // parse XFBML
+  });
+  FB.Event.subscribe('auth.authResponseChange', function(response) {
+    if (response.status === 'connected') {
+      testAPI();
+    } else if (response.status === 'not_authorized') {
+      FB.login();
+    } else {
+      FB.login();
+    }
+  });
+};
+
+// Load the SDK asynchronously
+(function(d){
+   var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
+   if (d.getElementById(id)) {return;}
+   js = d.createElement('script'); js.id = id; js.async = true;
+   js.src = "//connect.facebook.net/en_US/all.js";
+   ref.parentNode.insertBefore(js, ref);
+}(document));
+
+function testAPI() {
+  console.log('Welcome!  Fetching your information.... ');
+  FB.api('/me', function(response) {
+     console.log('Good to see you, ' + response.name + '.');
+  });
+}
+
