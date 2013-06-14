88188a362a0ec3f7150459c099f48912137f2d7d
proxima <proxima@botchris.(none)>
2013-06-14 15:15:54 -0400

    blah

--- app.rb
+++ app.rb
@@ -36,6 +36,7 @@ class Exit < ActiveRecord::Base
   belongs_to :source, :class_name => "Sector", :foreign_key => :source_sector_id
   belongs_to :destination, :class_name => "Sector", :foreign_key => :dest_sector_id
 
+  #north, east, south, west, jump
   DIRECTIONS = [ 0, 1, 2, 3, 4 ]
   COSTS = [ 1, 5 ]
 
