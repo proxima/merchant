479ff433f32ed163eca32689423d1f03794e12f6
Christopher Lee <f4milyp4nt@gmail.com>
2013-06-24 22:40:38 -0400

    Adding the local map function

--- app/models/sector.rb
+++ app/models/sector.rb
@@ -21,4 +21,44 @@ class Sector < ActiveRecord::Base
     return g[0] if g.size > 0
     return nil
   end
+
+  def to_rowcol
+    g = self.galaxy
+    position = self.id - g.min_sector_id
+    return position / g.dimension, position % g.dimension
+  end
+
+  def surrounding_sectors(radius)
+    g = self.galaxy
+
+    position = self.id - g.min_sector_id
+
+    row = position / g.dimension 
+    col = position % g.dimension
+
+    ret = []
+
+    mini_dimension = ((radius * 2) + 1)
+  
+    (-radius..radius).each do |row_radius|
+      cur_row = []
+      (-radius..radius).each do |col_radius|
+        new_row = (row + row_radius) % g.dimension 
+        new_col = (col + col_radius) % g.dimension
+
+        cur_row << (new_row * g.dimension) + new_col + g.min_sector_id
+      end
+      ret << cur_row
+    end
+
+    return ret
+  end
+
+  def self.test(id, radius)
+    s = Sector.find(id)
+    ret = s.surrounding_sectors(radius)
+    ret.each do |r|
+      p r
+    end
+  end
 end
